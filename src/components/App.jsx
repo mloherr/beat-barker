e3194acac9e2748a46e9bce23ad74086b0b8d7f0
mloherr <herraizmaria08@gmail.com>
2024-05-18 11:31:27 +0200

    First commit with template for React + Vite structure

---
+++ src/components/App.jsx
@@ -0,0 +1,17 @@
+import '../scss/App.scss';
+// import PropTypes from "prop-types";
+
+function App() {
+  return (
+    <>
+      <p>Hello, world!</p>
+    </>
+  );
+}
+
+// NombreDeMiComponente.propTypes = {
+//   nombreDeMiPropDeTipoStringOpcional: PropTypes.string,
+//   nombreDeMiPropDeTipoStringObligatoria: PropTypes.string.isRequired,
+// };
+
+export default App;
